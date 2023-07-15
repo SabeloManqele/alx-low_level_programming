977dcf296422df5c33fa0092a1be691d3c7275ff
SabeloManqele <sabelo.manqele93@gmail.com>
2023-07-15 13:07:08 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - numbers
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+
+int n;
+
+n = 0;
+
+while (n < 10)
+{
+putchar(n + '0');
+n++;
+}
+putchar('\n');
+return (0);
+}
